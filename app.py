33c05d68188dbab0b8e3dfef468919023242e454
calebfrome <calebfrome@ou.edu>
2018-12-03 15:47:30 -0600

    similarity analysis fully functional

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template
-
-import simple_queries
+from flask import Flask, render_template, request
 import similarity_queries
 
 app = Flask(__name__)
@@ -16,11 +14,19 @@ def bryan():
     return render_template('bryan.html', users=['Dragon', 'Aaron', 'Caleb'])
 
 
-@app.route('/caleb')
-def caleb():
-    similarity = similarity_queries.compute_similarity('Cholera', 'Ornithosis')
-    condition_names = similarity_queries.condition_names()
-    return render_template('caleb.html', similarity=similarity, conditions=condition_names)
+@app.route('/similarity')
+def similarity():
+    return render_template('similarity.html', conditions=similarity_queries.condition_names())
+
+
+@app.route('/similarity_results')
+def similarity_results():
+    data = request.args
+    condition1 = data['condition1']
+    condition2 = data['condition2']
+    similarity_data = similarity_queries.compute_similarity(condition1, condition2)
+    return render_template('similarity_results.html', condition1=condition1, condition2=condition2,
+                           similarity=similarity_data)
 
 
 if __name__ == '__main__':
